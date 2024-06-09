63b9e7e89f041c40fbe40c03a027faaa5107fb17
Yuta Takahashi <y.highbridge04@gmail.com>
2024-06-09 20:16:40 +0900

    refactor: Update homepage layout and profile image
    The code changes refactor the homepage layout by modifying the flexbox properties and adding a profile image. This improves the visual presentation and alignment of the content on the page.

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -3,13 +3,13 @@ import Image from "next/image";
 export default function Home() {
   return (
     <main className="flex flex-col min-h-screen items-center justify-center p-24">
-      <div className="flex">
+      <div className="flex max-md:flex-col items-center justify-center">
         <img
           src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQq9Sein3gZllZ5ycT5ycCOrki0p90TySVYKw&s"
           alt=""
           className="rounded-full w-auto h-auto"
         />
-        <div className="flex flex-col items-center justify-center m-10">
+        <div className="flex flex-col items-center justify-center py-10 md:px-10">
           <h1 className="text-4xl font-bold">Hello World</h1>
           <p className="text-lg">Welcome to my website</p>
         </div>
