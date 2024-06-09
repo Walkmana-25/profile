6299db9adcfefde256b9afdc92da7f29358646c6
Yuta Takahashi <y.highbridge04@gmail.com>
2024-06-09 22:05:33 +0900

    ```text feat: Add hamburger menu to navigation bar
    The code changes include adding a hamburger menu icon to the navigation bar on the homepage. This icon is provided by the "react-icons" package and improves the user experience by providing a visual indicator for the presence of a menu.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,6 +1,8 @@
 import type { Metadata } from "next";
 import { Inter } from "next/font/google";
+import { GiHamburgerMenu } from "react-icons/gi";
 import "./globals.css";
+import { IconContext } from "react-icons";
 
 const inter = Inter({ subsets: ["latin"] });
 
@@ -16,7 +18,20 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body className={inter.className}>{children}</body>
+      <body className={inter.className}>
+        <div className="bg-gradient-to-r from-slate-300 to-slate-200">
+          <nav className="w-full p-3 bg-transparent">
+            <div className="flex justify-between items-center">
+              <p className="text-lg">hi</p>
+              <p>hi</p>
+              <div className="mx-1">
+                <GiHamburgerMenu />
+              </div>
+            </div>
+          </nav>
+          {children}
+        </div>
+      </body>
     </html>
   );
 }
