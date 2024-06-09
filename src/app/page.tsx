8bebeff3124134707d0003ad835a0f7cf7d7d2ca
Yuta Takahashi <y.highbridge04@gmail.com>
2024-06-09 21:40:53 +0900

    feat: Update homepage with lazy loading for images

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -5,7 +5,7 @@ import { IconContext } from "react-icons";
 
 export default function Home() {
   return (
-    <main className="flex flex-col min-h-screen items-center justify-center p-24 bg-gradient-to-r from-slate-200 to-slate-250">
+    <main className="light flex flex-col min-h-screen items-center justify-center p-24 bg-gradient-to-r from-slate-200 to-slate-250">
       <div className="flex max-md:flex-col items-center justify-center">
         <img
           src="https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQq9Sein3gZllZ5ycT5ycCOrki0p90TySVYKw&s"
