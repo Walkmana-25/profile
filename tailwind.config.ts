6fcc39c81f437c4262485212a04afdc93c4c72db
Yuta Takahashi <y.highbridge04@gmail.com>
2024-06-10 12:37:49 +0900

    feat: Update homepage with lazy loading for images

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -14,7 +14,24 @@ const config: Config = {
           "conic-gradient(from 180deg at 50% 50%, var(--tw-gradient-stops))",
       },
     },
+    dark: {
+      "50": "#f6f6f6",
+      "100": "#e7e7e7",
+      "200": "#d1d1d1",
+      "300": "#b0b0b0",
+      "400": "#888888",
+      "500": "#6d6d6d",
+      "600": "#5d5d5d",
+      "700": "#4f4f4f",
+      "800": "#454545",
+      "900": "#3d3d3d",
+      "950": "#000000",
+    },
   },
   plugins: [require("daisyui")],
+  daisyui: {
+    themes: ["light", "dim"],
+    darkTheme: "dim",
+  },
 };
 export default config;
