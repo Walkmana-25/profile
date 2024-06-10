4520c11e2a9b337724b01637c3216b843f90d3a9
Yuta Takahashi <y.highbridge04@gmail.com>
2024-06-10 23:04:27 +0900

    feat: Update background colors and navigation bar in layout component
    The code changes update the background colors in the globals.css file to improve the visual aesthetics of the application. The foreground color is changed to black, and the background colors are updated to a lighter shade of gray for the light color scheme and a darker shade of blue for the dark color scheme. Additionally, the navigation bar in the layout component is refactored to improve its styling and responsiveness. This enhances the overall look and feel of the application and provides a better user experience.

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -44,51 +44,57 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={(inter.className, "h-screen", "w-screen")}>
-        <nav className="absolute flex w-full bg-inherit rounded justify-between items-center shadow-lg">
-          <div className="drawer">
-            <input id="my-drawer-3" type="checkbox" className="drawer-toggle" />
-            <div className="drawer-content flex flex-col">
-              {/* Navbar */}
-              <div className="w-full navbar bg-inherit">
-                <div className="flex-none lg:hidden">
-                  <label
-                    htmlFor="my-drawer-3"
-                    aria-label="open sidebar"
-                    className="btn btn-square btn-ghost"
-                  >
-                    <svg
-                      xmlns="http://www.w3.org/2000/svg"
-                      fill="none"
-                      viewBox="0 0 24 24"
-                      className="inline-block w-6 h-6 stroke-current"
+        <div className="w-full justify-center flex">
+          <nav className="absolute flex m-3 w-11/12 bg-inherit rounded justify-between items-center shadow-2xl border-b-2 dark:border-slate-800 border-gray-300">
+            <div className="drawer">
+              <input
+                id="my-drawer-3"
+                type="checkbox"
+                className="drawer-toggle"
+              />
+              <div className="drawer-content flex flex-col">
+                {/* Navbar */}
+                <div className="w-full navbar bg-inherit">
+                  <div className="flex-none lg:hidden">
+                    <label
+                      htmlFor="my-drawer-3"
+                      aria-label="open sidebar"
+                      className="btn btn-square btn-ghost"
                     >
-                      <path
-                        strokeLinecap="round"
-                        strokeLinejoin="round"
-                        strokeWidth="2"
-                        d="M4 6h16M4 12h16M4 18h16"
-                      ></path>
-                    </svg>
-                  </label>
-                </div>
-                <div className="flex-1 px-2 mx-2">
-                  <a className="btn btn-ghost">Hello World</a>
-                </div>
-                <div className="flex-none hidden lg:block">
-                  {NavbarContents("menu menu-horizontal")}
+                      <svg
+                        xmlns="http://www.w3.org/2000/svg"
+                        fill="none"
+                        viewBox="0 0 24 24"
+                        className="inline-block w-6 h-6 stroke-current"
+                      >
+                        <path
+                          strokeLinecap="round"
+                          strokeLinejoin="round"
+                          strokeWidth="2"
+                          d="M4 6h16M4 12h16M4 18h16"
+                        ></path>
+                      </svg>
+                    </label>
+                  </div>
+                  <div className="flex-1 px-2 mx-2">
+                    <a className="btn btn-ghost">Hello World</a>
+                  </div>
+                  <div className="flex-none hidden lg:block">
+                    {NavbarContents("menu menu-horizontal")}
+                  </div>
                 </div>
               </div>
+              <div className="drawer-side">
+                <label
+                  htmlFor="my-drawer-3"
+                  aria-label="close sidebar"
+                  className="drawer-overlay"
+                ></label>
+                {NavbarContents("menu p-4 w-80 min-h-full bg-base-200")}
+              </div>
             </div>
-            <div className="drawer-side">
-              <label
-                htmlFor="my-drawer-3"
-                aria-label="close sidebar"
-                className="drawer-overlay"
-              ></label>
-              {NavbarContents("menu p-4 w-80 min-h-full bg-base-200")}
-            </div>
-          </div>
-        </nav>
+          </nav>
+        </div>
 
         <div className="flex flex-col items-center justify-center h-screen w-screen">
           {children}
