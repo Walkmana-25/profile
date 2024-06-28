96eedd5521f556abaf8a7c9634f80c8dec08e2dc
Yuta Takahashi <y.highbridge04@gmail.com>
2024-06-28 19:40:34 +0900

    add timeline template

--- src/app/work/page.tsx
+++ src/app/work/page.tsx
@@ -1,3 +1,10 @@
+import { FaExternalLinkAlt } from "react-icons/fa";
+
+const timeLineContent = [
+  ["1984", "First Macintosh computer", "content"],
+  ["1999", "fff", "Ffff"],
+];
+
 export default function Page() {
   return (
     <main
@@ -10,27 +17,36 @@ export default function Page() {
       <div className="flex-col items-center justify-center">
         <h2 className="mb-4 text-center text-2xl">TIMELINE</h2>
         <ul className="timeline timeline-vertical">
-          <li>
-            <div className="timeline-start text-lg">1984</div>
-            <div className="timeline-middle">
-              <svg
-                xmlns="http://www.w3.org/2000/svg"
-                viewBox="0 0 20 20"
-                fill="currentColor"
-                className="h-5 w-5"
-              >
-                <path
-                  fillRule="evenodd"
-                  d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
-                  clipRule="evenodd"
-                />
-              </svg>
-            </div>
-            <div className="timeline-end timeline-box text-lg">
-              First Macintosh computer
-            </div>
-            <hr />
-          </li>
+          {timeLineContent.map((content) => (
+            <li>
+              <div className="timeline-start text-lg">{content.at(0)}</div>
+              <div className="timeline-middle">
+                <svg
+                  xmlns="http://www.w3.org/2000/svg"
+                  viewBox="0 0 20 20"
+                  fill="currentColor"
+                  className="h-5 w-5"
+                >
+                  <path
+                    fillRule="evenodd"
+                    d="M10 18a8 8 0 100-16 8 8 0 000 16zm3.857-9.809a.75.75 0 00-1.214-.882l-3.483 4.79-1.88-1.88a.75.75 0 10-1.06 1.061l2.5 2.5a.75.75 0 001.137-.089l4-5.5z"
+                    clipRule="evenodd"
+                  />
+                </svg>
+              </div>
+              <div className="timeline-end timeline-box text-lg w-3/4">
+                <details className="collapse collapse-arrow">
+                  <summary className="collapse-title text-xl font-medium">
+                    <p>{content.at(1)}</p>
+                  </summary>
+                  <div className="collapse-content">
+                    <p>{content.at(2)}</p>
+                  </div>
+                </details>
+              </div>
+              <hr />
+            </li>
+          ))}
         </ul>
       </div>
     </main>
